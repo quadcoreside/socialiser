c9c00dd01f6526e9e408f21db06c973df8e30a9d
Moussa <moussalfa94@gmail.com>
2021-05-30 21:07:04 +0200

    ok sync

--- app/src/main/java/com/wara/socialiser/ui/home/PostAdapater.kt
+++ app/src/main/java/com/wara/socialiser/ui/home/PostAdapater.kt
@@ -1,22 +1,27 @@
 package com.wara.socialiser.ui.home
 
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.wara.socialiser.R
-import com.wara.socialiser.data.response.Album
 import com.wara.socialiser.data.response.Post
+import com.wara.socialiser.ui.post.PostViewActivity
 
 
-class PostAdapater(private var dataSet: List<Post>) : RecyclerView.Adapter<PostAdapater.ViewHolder>() {
+class PostAdapater(private var dataSet: List<Post>) : RecyclerView.Adapter<PostAdapater.PostListViewHolder>() {
+    val EXTRA_MESSAGE = "com.wara.socialiser.MESSAGE"
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class PostListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val titleTextView: TextView
         val descriptionTextView: TextView
         private var image: ImageView? = null
@@ -30,34 +35,82 @@ class PostAdapater(private var dataSet: List<Post>) : RecyclerView.Adapter<PostA
         fun getImage(): ImageView? {
             return image
         }
+
+        var itemClick: ((Post) -> Unit)? = null
+        var onItemLongPress: ((Post) -> Unit)? = null
+
+        fun bindView(post: Post) {
+            titleTextView.text = post.title;
+            descriptionTextView.text = post.body;
+
+            /* Chargement de l'image avec Glide */
+            /* dataSet[position].thumbnailUrl on affiche la meme image pour tout les posts */
+            Glide.with(itemView.context)
+                .load("https://www.esiea.fr/wp-content/uploads/2016/04/Logo-ESIEA.jpg").centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(getImage());
+
+            //For Handling RecyclerView Item Click
+            itemView?.setOnClickListener {
+                //invoke() function will pass the value to receiver function.
+                itemClick?.invoke(post)
+            }
+
+            //For handling RecyclerView Item Long Click
+            /*itemView?.setOnLongClickListener {
+                onItemLongPress?.invoke(post)
+                return@setOnLongClickListener true
+            }*/
+        }
+
     }
+
     fun updateList(list: List<Post>){
         dataSet = list
         notifyDataSetChanged()
     }
     private var context: Context? = null
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): PostListViewHolder {
+        /*
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.media_item, viewGroup, false)
+        */
 
-        /* On recuperer le context de cette maniere */
-        context = viewGroup.context;
+        return PostListViewHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout.media_item, viewGroup, false)).apply {
+            itemClick = { post ->
+                //this.itemClick?.invoke(post)
 
-        return ViewHolder(view)
+                /*
+                val message = "id = " + post.id
+                val intent = Intent(viewGroup.context, PostViewActivity::class.java).apply {
+                    putExtra(EXTRA_MESSAGE, message)
+                }*/
+
+                Log.d("TAG", "id = " + post.id)
+
+                val intent = Intent(viewGroup.context, PostViewActivity::class.java)
+                intent.putExtra("post_id", post.id)
+                viewGroup.context.startActivity(intent)
+            }
+            onItemLongPress = { post ->
+                //this.onItemLongPress?.invoke(post)
+            }
+        }
     }
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.titleTextView.text = dataSet[position].title;
-        viewHolder.descriptionTextView.text = dataSet[position].body;
 
-        /* Chargement de l'image avec Glide */
-        /* dataSet[position].thumbnailUrl on affiche la meme image pour tout les posts */
-        Glide.with(context)
-            .load("https://www.esiea.fr/wp-content/uploads/2016/04/Logo-ESIEA.jpg").centerCrop()
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .into(viewHolder.getImage());
+    override fun onBindViewHolder(viewHolder: PostListViewHolder, position: Int) {
+        val movieViewHolder = viewHolder as PostListViewHolder
+        movieViewHolder.bindView(dataSet[position])
     }
 
-    override fun getItemCount() = dataSet.size
+    private val limit = 20
+    override fun getItemCount(): Int {
+        if(dataSet.size > limit){
+            return limit;
+        } else {
+            return dataSet.size;
+        }
+    }
 
 }
